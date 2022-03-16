afa0364f25a81926a2d86ec9ee6e7b26025d2d8d
keshav12-deloitte <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-16 13:35:31 +0530

    Update Animal.java

--- question 2/Animal.java
+++ question 2/Animal.java
@@ -2,4 +2,30 @@ public class Animal {
     private int a;
     private int b;
     private int c;
+    public void seta(int a)
+    {
+        this.a = a;
+    }
+    public int geta()
+    {
+        return a;
+    }
+
+    public void setb(String b)
+    {
+        this.b = b;
+    }
+    public String getb()
+    {
+        return b;
+    }
+
+    public void setc(int c)
+    {
+        this.c = c;
+    }
+    public int getc()
+    {
+        return c;
+    }
 }
