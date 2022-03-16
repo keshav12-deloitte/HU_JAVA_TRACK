ec7369d0991bf07fdd9a06acc50d517b92afbdea
keshav12-deloitte <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-16 14:46:57 +0530

    Update My.java

--- My.java
+++ My.java
@@ -1,15 +1,10 @@
 public class My {
     public static void main(String[] args) {
-        Area obj = new Area();
-        obj.radius = 5;
-        System.out.println("area of circle is");
-        obj.area();
-        System.out.println("area of square is");
-
-        obj.area(5);
-        System.out.println("area of rectangle is");
-        obj.area(6, 6);
-
-
+       Area obj = new Area();
+       obj.radius=5;
+       obj.area();
+       obj.area(5);
+       obj.area(6,6);
+       obj.area(5,6,0.5);
     }
 }
