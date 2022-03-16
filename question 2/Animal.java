028255d528d25c28911d7be35e91799fa9d3bab1
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-16 13:56:34 +0530

    third commit

--- question 2/Animal.java
+++ question 2/Animal.java
@@ -1,5 +1,37 @@
+package HU_JAVA_TRACK;
+
 public class Animal {
     private int a;
-    private int b;
+    private String b;
     private int c;
+
+
+    public void seta(int a)
+    {
+        this.a = a;
+    }
+    public int geta()
+    {
+        return a;
+    }
+
+    public void setb(String b)
+    {
+        this.b = b;
+    }
+    public String getb()
+    {
+        return b;
+    }
+
+    public void setc(int c)
+    {
+        this.c = c;
+    }
+    public int getc()
+    {
+        return c;
+    }
+
 }
+
