7d2f69d8f7d9cf6f07980155c8b56d75b7fd756a
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-18 19:48:24 +0530

    resolved the conflicts

--- Animals.java
+++ Animals.java
@@ -4,27 +4,37 @@ public class Animals {
     private int c;
 
 
-    public void seta(int a) {
-        this.a = a;
+    public void seta(int a1) {
+        int a = a1 ;
+        this.a = a1;
     }
 
     public int geta() {
         return a;
     }
 
-    public void setb(String b) {
-        this.b = b;
+    public void setb(String b1) {
+        String b = b1;
+        this.b = b1;
     }
 
     public String getb() {
         return b;
     }
 
-    public void setc(int c) {
-        this.c = c;
+    public void setc(int c1) {
+        int c =c1;
+        this.c = c1;
     }
 
     public int getc() {
         return c;
+    }
+    public static void main(String[] args) {
+        Animals obj2 = new Animals();
+        obj2.seta(101);
+        obj2.setb("Keshav");
+        obj2.setc(18);
+        System.out.println(obj2.geta()+" "+obj2.getb()+" "+obj2.getc());
     }
 }
