f32b2159b8a515d599acb6d8557b1ab152565693
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-18 19:58:07 +0530

    remved the duplicates

--- Create.java
+++ Create.java
@@ -1,11 +1,11 @@
 public class Create extends Animals {
 
     public static void main(String[] args) {
-            Animals obj2 = new Animals();
-            obj2.seta(101);
-            obj2.setb("Keshav");
-            obj2.setc(18);
+        Animals obj2 = new Animals();
+        obj2.seta(101);
+        obj2.setb("Keshav");
+        obj2.setc(18);
         System.out.println(obj2.geta()+" "+obj2.getb()+" "+obj2.getc());
-        }
+    }
     }
 
